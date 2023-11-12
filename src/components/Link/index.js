b79ccb6980e86baf17a097eab2a6bd2a049e488c
Murilo Principe <murilo.rprincipe2021@outlook.com>
2023-11-12 17:01:27 -0300

    first commit

---
+++ src/components/Link/index.js
@@ -0,0 +1,9 @@
+import NextLink from 'next/link';
+import { Text } from '../../theme/components';
+
+export default function Link({ children, href, ...props}) {
+    return (
+        <NextLink href={href} passHref>
+        </NextLink>
+    );
+}
